6720ca3359e8387d4a69c6a4b72c92bc87d46d1a
Daniel Nguyen <danielnguyen@Daniels-MacBook-Pro.local>
2019-05-05 15:28:36 +0700

    ADd Expensify styles

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
@@ -9,8 +10,13 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
     
     return (
-        <div>
-            <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{expenseCount} </span> {expenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
         </div>
 
     );
@@ -25,47 +31,3 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps)(ExpensesSummary);
-
-// import React from 'react';
-// import { connect } from 'react-redux';
-// import getVisibleExpenses from '../selectors/expenses';
-
-// class ExpensesSummary extends React.Component {
-//     constructor(props) {
-//         super(props);
-
-//         this.state = {
-//             expenses: props.expenses,
-//             filters: props.filters
-//         } 
-//     }
-
-//     getVisibleExpensesCount = (visibleExpenses) => {
-//         return visibleExpenses.length;
-//     }
-
-//     getVisibleExpensesTotal = (visibleExpenses) => {
-//         return visibleExpenses
-//             .map((expense) => expense.amount)
-//             .reduce((sum, value) => sum + value, 0);
-//     }
-
-//     render() {
-//         const visibleExpenses = getVisibleExpenses(this.state.expenses, this.state.filters);
-//         return (
-//             <div>
-//                 <p>Visible expenses count: {this.getVisibleExpensesCount(visibleExpenses)}</p>
-//                 <p>Total visible expenses: {this.getVisibleExpensesTotal(visibleExpenses)}</p>
-//             </div>            
-//         );
-//     }
-// }
-
-// const mapStateToProps = (state, props) => {
-//     return {
-//         expenses: state.expenses,
-//         filters: state.filters
-//     }
-// }
-
-// export default connect(mapStateToProps)(ExpensesSummary);
